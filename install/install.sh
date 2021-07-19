6950f9816f4ef319161a0dce6dd439a1e69a8509
Ecoriap <ecoriap@protonmail.com>
2021-07-19 20:21:19 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -11,12 +11,13 @@ echo -e "| (      | |      | |   | || (\ (      | |   | (   ) |"
 echo -e "| (____/\| (____/\| (___) || ) \ \_____) (___| )   ( |" 
 echo -e "(_______/(_______/(_______)|/   \__/\_______/|/     \|"
 echo
-#---------------------------------------------------------------#
-#  Nom du projet: EcoWebHost                                                           #
-#  Créateur: Pierric Buchez                                                            #
-#  Date de début du projet: 19/07/2021  
-#  Créateur: Pier#                                                      #
-#---------------------------------------------------------------#
+echo -e "#-----------------------------------------------------#
+echo -e "#  Nom du projet: EcoWebHost                          #
+echo -e "#  Créateur: Pierric Buchez                           #
+echo -e "#  Date de début du projet: 19/07/2021                #
+echo -e "#  Description: EcoWebHost est une blockchain.        #
+echo -e "#               reposant sur le Proof of host.        #
+echo -e "#-----------------------------------------------------#
 
 echo -e "\n\n"
 
